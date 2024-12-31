07d1b9cee2d085bb225627d383053d37a1da2396
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2024-12-31 16:26:22 +0400

    Animating Characters
    Added .anim files - creating a "Hop" and "Shiver" animation.

--- Visual Novel Project/Assets/_MAIN/Scripts/Core/Characters/Character.cs
+++ Visual Novel Project/Assets/_MAIN/Scripts/Core/Characters/Character.cs
@@ -14,7 +14,7 @@ namespace CHARACTERS
         public const bool ENABLE_ON_START = true;
         private const float UNHIGHLIGHTED_DARKEN_STRENGTH = 0.65f;
         public const bool DEFAULT_ORIENTATION_IS_FACING_LEFT = true;
-
+        public const string ANIMATION_REFRESH_TRIGGER = "Refresh";
 
         public string name = "";
         public string displayName = "";
@@ -276,6 +276,18 @@ namespace CHARACTERS
             if (autoSortCharactersOnUI)
                 charactermanager.SortCharacters();
         }
+        
+        public void Animate(string animation)
+        {
+            animator.SetTrigger(animation);
+        }
+
+        public void Animate(string animation, bool state)
+        {
+            animator.SetBool(animation, state);
+            animator.SetTrigger(ANIMATION_REFRESH_TRIGGER);
+        }
+
         public enum CharacterType
         {
             Text,
--- Visual Novel Project/Assets/_TESTING/Scripts/TestCharacters.cs
+++ Visual Novel Project/Assets/_TESTING/Scripts/TestCharacters.cs
@@ -26,34 +26,34 @@ namespace TESTING
 
         IEnumerator Test()
         {
-            Character_Sprite Rainer = CreateCharacter("Rainer") as Character_Sprite;
-            Character_Sprite EvilRainer = CreateCharacter("EvilRainer as Rainer") as Character_Sprite;
+            //Character_Sprite Rainer = CreateCharacter("Rainer") as Character_Sprite;
+            //Character_Sprite EvilRainer = CreateCharacter("EvilRainer as Rainer") as Character_Sprite;
             Character_Sprite Baron = CreateCharacter("Baron") as Character_Sprite;
             Character_Sprite Ali = CreateCharacter("Ali") as Character_Sprite;
 
-            EvilRainer.SetColor(Color.red);
-
-            Baron.SetPosition(new Vector2(0.3f, 0));
-            Ali.SetPosition(new Vector2(0.45f, 0));
-            Rainer.SetPosition(new Vector2(0.6f, 0));
-            EvilRainer.SetPosition(new Vector2(0.75f, 0));
-
-            EvilRainer.SetPriority(1000);
-            Ali.SetPriority(15);
-            Baron.SetPriority(8);
-            Rainer.SetPriority(30);
+            Baron.SetPosition(new Vector2(0, 0));
+            Ali.SetPosition(new Vector2(1, 0));
 
             yield return new WaitForSeconds(1);
 
-            CharacterManager.instance.SortCharacters(new string[] { "Ali", "Baron" });
+            // Ali.TransitionSprite(Ali.GetSprite("Mogging"));
+            Ali.Animate("Hop");
+            yield return Ali.Say("Hey Baron.. {wa 1} Do you feel that chill?");
 
-            yield return new WaitForSeconds(1);
+            Baron.FaceRight();
+            Baron.TransitionSprite(Baron.GetSprite("A2"));
+            Baron.TransitionSprite(Baron.GetSprite("A_Shocked"), layer: 1);
+            Baron.MoveToPosition(new Vector2(0.1f, 0));
+            Baron.Animate("Shiver", true);
+            yield return Baron.Say("I- {wa 0.5} I don't know - but I hate it! {a} It's freezing!");
 
-            CharacterManager.instance.SortCharacters();
+            Ali.TransitionSprite(Ali.GetSprite("Happy"));
+            yield return Ali.Say("Oh, {wa 0.5} it's over!");
 
-            yield return new WaitForSeconds(1);
-
-            CharacterManager.instance.SortCharacters(new string[] { "Baron", "EvilRainer", "Rainer", "Ali" });
+            Baron.TransitionSprite(Baron.GetSprite("B2"));
+            Baron.TransitionSprite(Baron.GetSprite("B_Blush2"), layer: 1);
+            Baron.Animate("Shiver", false);
+            yield return Baron.Say("Thank the Lord... {wa 0.5} I'm not wearing enough clothes for that crap...");
 
             yield return null;
         }
