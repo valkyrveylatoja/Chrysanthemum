8f785c4a48236755ead6833aec2f60e01bdbcd62
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2025-01-01 17:32:36 +0400

    Character Command Integration

--- Visual Novel Project/Assets/_MAIN/Scripts/Core/Commands/Database/CMD_DatabaseExtension.cs
+++ Visual Novel Project/Assets/_MAIN/Scripts/Core/Commands/Database/CMD_DatabaseExtension.cs
@@ -7,5 +7,7 @@ namespace COMMANDS
     public abstract class CMD_DatabaseExtension // Abstract meaning it cannot be instanced
     {
         public static void Extend(CommandDatabase database) { }
+
+        public static CommandParameters ConvertDataToParameters(string[] data) => new CommandParameters(data);
     }
 }
