73331f6916bb50bde58b6b09770e32bdb9963279
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2025-01-04 12:27:30 +0400

    Audio Manager Setup

---
+++ Visual Novel Project/Assets/_MAIN/Scripts/Core/Audio/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+
+    public AudioMixerGroup musicMixer;
+    public AudioMixerGroup sfxMixer;
+    public AudioMixerGroup voiceMixer;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            transform.SetParent(null);
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+    }
+}
