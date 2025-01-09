2f00ee4ff265934ba9233d686cbafe29cbf7a6d5
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2025-01-09 10:52:00 +0400

    Operator Lines

--- Visual Novel Project/Assets/_MAIN/Scripts/Core/Dialogue/TagManager.cs
+++ Visual Novel Project/Assets/_MAIN/Scripts/Core/Dialogue/TagManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public class TagManager
@@ -15,19 +16,55 @@ public class TagManager
     };
     private static readonly Regex tagRegex = new Regex("<\\w+>");
 
-    public static string Inject(string text)
+    public static string Inject(string text, bool injectTags = true, bool injectVariables = true)
     {
-        if (tagRegex.IsMatch(text))
+        if (injectTags)
+            text = InjectTags(text);
+
+        if (injectVariables)
+            text = InjectVariables(text);
+
+        return text;
+    }
+
+    private static string InjectTags(string value)
+    {
+        if (tagRegex.IsMatch(value))
         {
-            foreach (Match match in tagRegex.Matches(text))
+            foreach (Match match in tagRegex.Matches(value))
             {
                 if (tags.TryGetValue(match.Value, out var tagValueRequest))
                 {
-                    text = text.Replace(match.Value, tagValueRequest());
+                    value = value.Replace(match.Value, tagValueRequest());
                 }
             }
         }
 
-        return text;
+        return value;
+    }
+
+    public  static string InjectVariables(string value)
+    {
+        var matches = Regex.Matches(value, VariableStore.REGEX_VARIABLE_IDS);
+        var matchesList = matches.Cast<Match>().ToList();
+
+        for (int i = matchesList.Count - 1; i >= 0; i--)
+        {
+            var match = matchesList[i];
+            string variableName = match.Value.TrimStart(VariableStore.VARIABLE_ID);
+
+            if (!VariableStore.TryGetValue(variableName, out object variableValue))
+            {
+                UnityEngine.Debug.LogError($"Variable {variableName} not found in string assignment");
+                continue;
+            }
+
+            int lengthToBeRemoved = match.Index + match.Length > value.Length ? value.Length - match.Index : match.Length;
+
+            value = value.Remove(match.Index, lengthToBeRemoved);
+            value = value.Insert(match.Index, variableValue.ToString());
+        }
+
+        return value;
     }
 }
